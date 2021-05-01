640ee405e02f53e6dd79e11939f774369b4fdf7b
shirayner <1766211120@qq.com>
2021-05-01 21:46:50 +0800

    feat: add plugin cursor-effects

--- docs/.vuepress/config/plugins/index.js
+++ docs/.vuepress/config/plugins/index.js
@@ -42,7 +42,7 @@ module.exports = [
     "cursor-effects",
     {
       size: 2, // size of the particle, default: 2
-      shape: ["star" | "circle"], // shape of the particle, default: 'star'
+      shape: ["star"], // shape of the particle, default: 'star'
       zIndex: 999999999, // z-index property of the canvas, default: 999999999
     },
   ],
